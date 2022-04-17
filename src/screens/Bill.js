d04e1be89b811cf67d178f78942a9237e2c39f5a
ParmarTarun <parmartarun.dev@gmail.com>
2022-04-17 20:36:43 +0530

    :sparkles: Add Show Current Bill Feature

--- src/screens/Bill.js
+++ src/screens/Bill.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Pressable, StyleSheet, Text, Linking} from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+const showCurrentBill = () => {
+  Linking.openURL('https://bill-251a9.web.app/?bill=true');
+};
 
 const Bill = () => {
   return (
     <View style={styles.screen}>
-      <Text>Bills</Text>
+      <Pressable style={styles.button} onPress={showCurrentBill}>
+        <Text style={styles.text}>
+          Show Current Bill
+          <Icon style={styles.icon} name="external-link" size={24} />
+        </Text>
+      </Pressable>
     </View>
   );
 };
@@ -12,8 +22,24 @@ const Bill = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent:"center",
-    alignItems:"center"
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  button: {
+    margin: 20,
+    backgroundColor: 'grey',
+    width: '80%',
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 18,
+  },
+  icon: {
+    padding:10
   },
 });
 
